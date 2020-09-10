5cc1710f4dbcf56df90c9ae63973b4d31eaf80d6
James Termngam <ac94595@avcol.school.nz>
2020-09-10 14:45:50 +1200

    added aboutus & pics

--- Authsystem/Controllers/HomeController.cs
+++ Authsystem/Controllers/HomeController.cs
@@ -30,6 +30,11 @@ namespace Authsystem.Controllers
             return View();
         }
 
+        public IActionResult Aboutus()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
